c42fde743c7c7a3b3e7d7c87e4aeb4a367aba882
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-08-08 07:50:59 +0530

    initialized

---
+++ src/app/blog/[id]/layout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const BlogLayout = ({ children }) => {
+  return (
+    <div>
+      <h1>Hi this is a blog</h1>
+      {children}
+    </div>
+  );
+};
+export default BlogLayout;
